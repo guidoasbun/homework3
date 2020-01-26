fe68d4bba12dcd88a9c0415bf1d7da4b2f6df0df
Guido <Asbun>
2020-01-26 11:29:05 -0800

    using for loop

--- script.js
+++ script.js
@@ -1,58 +1,78 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// Assignment Code
+  var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+let passwordGenerator = function () {
+  let passwordLength = prompt('Please enter a password length from 8 - 128')
+  passwordLength = +passwordLength
+  let includeLower = confirm('Include lower case?')
+  let includeUpper = confirm('Include upper case?')
+  let includeNumber = confirm('Include numbers?')
+  let includeSymbols = confirm('Include Symbols?')
+  let passwordString = ''
+  let password = ''
 
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-
-let passwordLength = prompt('please enter a password length from 8 - 128')
-passwordLength = +passwordLength
-let includeLower = confirm('include lower case?')
-let includeUpper = confirm('include upper case?')
-let includeNumber = confirm('include numbers?')
-let includeSymbols = confirm('include Symbols?')
-
-
-function getRandomUpper() {
-  const lowerLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  return lowerLetter[Math.floor(Math.random() * lowerLetter.length)]
-}
-
-function getRandomLower() {
-  const upperLetter = 'abcdefghijklmnopqrstuvwxyz'
-  return upperLetter[Math.floor(Math.random() * upperLetter.length)]
-}
-
-function getRandomNumber() {
-  const randomNumber = '0123456789'
-  return randomNumber[Math.floor(Math.random() * randomNumber.length)]
-}
-
-function getRandomSymbol() {
-  const randomSymbol = '!@#$%^&*()_+'
-  return randomSymbol[Math.floor(Math.random() * randomSymbol.length)]
-}
-
-function passwordGenerator(length) {
-  let password= ''
-  let i=0
-    while (i < length){
-      password += getRandomUpper()
-      i++
-      password += getRandomLower()
-      i++
-      password += getRandomNumber()
-      i++
-      password += getRandomSymbol()
+  function getRandomUpper() {
+    const lowerLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return lowerLetter[Math.floor(Math.random() * lowerLetter.length)]
+  }
+  function getRandomLower() {
+    const upperLetter = 'abcdefghijklmnopqrstuvwxyz'
+    return upperLetter[Math.floor(Math.random() * upperLetter.length)]
+  }
+  function getRandomNumber() {
+    const randomNumber = '0123456789'
+    return randomNumber[Math.floor(Math.random() * randomNumber.length)]
+  }
+  function getRandomSymbol() {
+    const randomSymbol = '!@#$%^&*()_+'
+    return randomSymbol[Math.floor(Math.random() * randomSymbol.length)]
+  }
+  
+  for (let i = 0; i < passwordLength; i ++){
+    if (includeLower){
+      password = passwordString.concat(getRandomLower())
     }
+    if (includeUpper){
+      password = passwordString.concat(getRandomUpper())
+    }
+    if (includeNumber){
+      password = passwordString.concat(getRandomNumber())
+    }
+    if (includeSymbols){
+      password = passwordString.concat(getRandomSymbol())
+    }
+  } 
+
+  
+
+  
+    
+
+
+
+
+
     return password
 }
-console.log(passwordGenerator(passwordLength))
+
+ 
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = passwordGenerator();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Password generator function
+
+
+
+// console.log(passwordGenerator(passwordLength, includeLower, includeUpper, includeNumber, includeSymbols))
+
